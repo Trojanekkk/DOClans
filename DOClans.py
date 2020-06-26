71f09320076f58012d666f67399f6fc36a7f783a
Wojciech Trojanowski <W.Trojan27@gmail.com>
2020-06-26 11:41:06 +0200

    Fix: Some issues; Add: Own cred to grab info; Save info every 25 checks

--- DOClans.py
+++ DOClans.py
@@ -4,9 +4,14 @@ import pandas as pd
 import json, codecs
 import time
 
-print('Starting script...')
+def isEmpty (clanAttrArr):
+    if (len(clanAttrArr) > 0):
+        return clanAttrArr[0]
+    return False
 
 clans = list()
+
+print('Starting script...')
 
 session_requests = requests.session()
 
@@ -15,13 +20,21 @@ result = session_requests.get(login_url)
 
 print('Getting login info and tokens...')
 
+print("Your credentials won't be saved anywhere, it's only required to grab clans info")
+username = input("Username: ")
+password = input("Password: ")
+server = input("Server (pl2): ")
+
+if not server:
+    server = 'pl2'
+
 tree = html.fromstring(result.text)
 auth_token = list(set(tree.xpath("//input[@name='reloadToken']/@value")))[0]
 login_destination = list(set(tree.xpath("//form[@name='bgcdw_login_form']/@action")))[0]
 
 payload = {
-    'username': '',
-    'password': '',
+    'username': username,
+    'password': password,
     'reloadToken': auth_token
 }
 print('logging in...')
@@ -35,8 +48,8 @@ print('Logging in DONE\n')
 
 for i in range(1,10):
     time.sleep(1)
-    print('checking id: ' + str(i))
-    url = 'https://pl2.darkorbit.com/indexInternal.es?action=internalNewClanDetails&clanId=' + str(i)
+    print('checking id: ' + str(i) + '     ', end="", flush=True)
+    url = 'https://' + str(server) + '.darkorbit.com/indexInternal.es?action=internalNewClanDetails&clanId=' + str(i)
     result = session_requests.get(
         url,
         headers = dict(referer=login_url)
@@ -44,29 +57,40 @@ for i in range(1,10):
 
     tree = html.fromstring(result.text)
     
-    clanName = list(set(tree.xpath("//li[@class='clan_item']/span[text() = 'Nazwa klanu']/following-sibling::span/text()")))
-    if (len(clanName) > 0):
-        clanTag = list(set(tree.xpath("//li[@class='clan_item']/span[text() = 'Tag klanu']/following-sibling::span/text()")))
-        clanDate = list(set(tree.xpath("//li[@class='clan_item']/span[text() = 'Data założenia']/following-sibling::span/text()")))
-        clanOwner = list(set(tree.xpath("//li[@class='clan_item']/span[text() = 'Zarządca']/following-sibling::span/text()")))
-        clanMembers = list(set(tree.xpath("//li[@class='clan_item']/span[text() = 'Liczba członków']/following-sibling::span/text()")))
-        clanRank = list(set(tree.xpath("//li[@class='clan_item']/span[text() = 'Ranga klanu']/following-sibling::span/text()")))
-        clanCompany = list(set(tree.xpath("//li[@class='clan_item']/span[text() = 'Firma']/following-sibling::span/text()")))
-        clanStatus = list(set(tree.xpath("//li[@class='clan_item']/span[text() = 'Status']/following-sibling::span/text()")))
+    clanName = isEmpty(list(set(tree.xpath("//li[@class='clan_item']/span[text() = 'Nazwa klanu']/following-sibling::span/text()"))))
+
+    if (clanName):     
+        clanTag = isEmpty(list(set(tree.xpath("//li[@class='clan_item']/span[text() = 'Tag klanu']/following-sibling::span/text()"))))
+        clanDate = isEmpty(list(set(tree.xpath("//li[@class='clan_item']/span[text() = 'Data założenia']/following-sibling::span/text()"))))
+        clanOwner = isEmpty(list(set(tree.xpath("//li[@class='clan_item']/span[text() = 'Zarządca']/following-sibling::span/text()"))))
+        clanMembers = isEmpty(list(set(tree.xpath("//li[@class='clan_item']/span[text() = 'Liczba członków']/following-sibling::span/text()"))))
+        clanRank = isEmpty(list(set(tree.xpath("//li[@class='clan_item']/span[text() = 'Ranga klanu']/following-sibling::span/text()"))))
+        clanCompany = isEmpty(list(set(tree.xpath("//li[@class='clan_item']/span[text() = 'Firma']/following-sibling::span/text()"))))
+        clanStatus = isEmpty(list(set(tree.xpath("//li[@class='clan_item']/span[text() = 'Status']/following-sibling::span/text()"))))
         
         clan = {
             'id': i,
-            'clanName': clanName[0],
-            'clanTag': clanTag[0],
-            'clanDate': clanDate[0],
-            'clanOwner': clanOwner[0],
-            'clanCompany': clanCompany[0],
-            'clanRank': clanRank[0],
-            'clanMembers': clanMembers[0],
-            'clanStatus': clanStatus[0]
+            'clanName': clanName,
+            'clanTag': clanTag,
+            'clanDate': clanDate,
+            'clanOwner': clanOwner,
+            'clanCompany': clanCompany,
+            'clanRank': clanRank,
+            'clanMembers': clanMembers,
+            'clanStatus': clanStatus
         }
         clans.append(clan)
-        print(clan)
+
+        print('found')
+    else:
+        print()
+
+    if (i % 25 == 0):
+        with open('clans.json', 'wb') as f:
+            json.dump(clans, codecs.getwriter('utf-8')(f), ensure_ascii=False)
+
+        df =  pd.DataFrame.from_dict(clans)
+        df.to_csv('clans.csv')
 
 with open('clans.json', 'wb') as f:
     json.dump(clans, codecs.getwriter('utf-8')(f), ensure_ascii=False)
